6e4331cfd1b6a55ca8fca37cea2abdb05208d02a
Julien B <julien@toshokan.fr>
2019-03-23 10:40:15 +0100

    Adding a PostGreSQL DB instance

--- terraform/databases.tf
+++ terraform/databases.tf
@@ -1,6 +1,8 @@
 # We'll spawn here the needed databases
 
+###################################
 # Redis (needed for Nameko Product)
+###################################
 
 resource "aws_security_group" "redis_sg" {
   vpc_id = "${module.vpc.vpc_id}"
@@ -12,28 +14,6 @@ resource "random_string" "auth_token" {
   length  = 64
   special = false
 }
-
-/*
-module "redis" {
-  source             = "cloudposse/elasticache-redis/aws"
-  version            = "0.9.0"
-  namespace          = "general"
-  name               = "redis"
-  stage              = "dev"
-  security_groups    = ["${aws_security_group.redis_sg.id}"]
-  auth_token         = "${random_string.auth_token.result}"
-  vpc_id             = "${module.vpc.vpc_id}"
-  subnets            = "${module.vpc.elasticache_subnets}"
-  maintenance_window = "wed:03:00-wed:04:00"
-  cluster_size       = "1"
-  instance_type      = "cache.t2.micro"
-  engine_version     = "4.0.10"
-  apply_immediately  = "true"
-
-  availability_zones = "${var.availability_zones}"
-  automatic_failover = "false"
-}
-*/
 
 resource "aws_elasticache_subnet_group" "redis_subnet" {
   name       = "nameko-redis-subnet"
@@ -52,12 +32,6 @@ resource "aws_elasticache_cluster" "redis" {
   subnet_group_name    = "${aws_elasticache_subnet_group.redis_subnet.name}"
 }
 
-/*
-output "nodes" {
-  value = "${lookup(aws_elasticache_cluster.redis.cache_nodes[0], "address")}"
-}
-*/
-
 resource "aws_security_group_rule" "redis_allow_products_service" {
   type                     = "ingress"
   to_port                  = 6379
@@ -65,4 +39,42 @@ resource "aws_security_group_rule" "redis_allow_products_service" {
   source_security_group_id = "${module.services.redis_sg}"
   from_port                = 6379
   security_group_id        = "${aws_security_group.redis_sg.id}"
+}
+
+#######################################
+# PostGreSQL (needed for Nameko Orders)
+#######################################
+
+resource "aws_security_group" "postgresql_sg" {
+  vpc_id = "${module.vpc.vpc_id}"
+  name   = "PostGreSQLSGForNamekoExamples"
+  tags   = "${local.tags}"
+}
+
+resource "aws_db_subnet_group" "postgre_subnet" {
+  name       = "nameko-postgre-subnet"
+  subnet_ids = ["${module.vpc.database_subnets}"]
+  tags = "${local.tags}"
+}
+
+resource "aws_db_instance" "postgresql" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = "9.6.6"
+  instance_class       = "db.t2.micro"
+  name                 = "postgresqlnameko"
+  username             = "superuser"
+  password             = "${random_string.auth_token.result}"
+  parameter_group_name = "default.postgres9.6"
+  tags = "${local.tags}"
+}
+
+resource "aws_security_group_rule" "postgre_allow_orders_service" {
+  type                     = "ingress"
+  to_port                  = 5432
+  protocol                 = "-1"
+  source_security_group_id = "${module.services.orders_sg}"
+  from_port                = 5432
+  security_group_id        = "${aws_security_group.postgresql_sg.id}"
 }
